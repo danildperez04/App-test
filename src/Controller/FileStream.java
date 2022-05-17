c0a1966b24728f308dc213ad4dd5fa21ec6828d5
danildperez04 <gamerdan589@gmail.com>
2022-05-17 09:48:06 -0600

    Password Encrypting

---
+++ src/Controller/FileStream.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import Model.User;
+
+public class FileStream {
+    
+    public User getObject(){
+        return new User();
+    }
+}
---
+++ src/Model/User.java
@@ -0,0 +1,21 @@
+package Model;
+
+import static Controller.Encryption.encrypt;
+import java.security.GeneralSecurityException;
+import java.util.Scanner;
+import javax.crypto.spec.SecretKeySpec;
+
+public class User {
+    
+    private static void passwordCrypted() throws GeneralSecurityException{
+
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    private String nombre;
+    private static String password;
+    private static Scanner sc = new Scanner(System.in);
+}
