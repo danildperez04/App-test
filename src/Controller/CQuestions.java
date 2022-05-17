bc8d23f9eae01918f2962ef5babe9f9acc2f5b0e
danildperez04 <gamerdan589@gmail.com>
2022-05-17 08:41:54 -0600

    Implementacion para diferentes tests

--- src/Controller/CQuestions.java
+++ src/Controller/CQuestions.java
@@ -11,30 +11,16 @@ import java.util.Set;
 import javax.swing.JButton;
 
 public class CQuestions {
-    public CQuestions(FrmQuestions view, Question model){
+    public CQuestions(FrmQuestions view, Question model, Test test){
         this.view = view;
         this.model = model;
+        this.testModel = test;
         this.btnAnswers = new JButton[]{view.BtnResp1, view.BtnResp2, view.BtnResp3, view.BtnResp4};
-        initQuestions();
-        getSequence(englishTestModel.getQuestions().size());
-        System.out.println(englishTestModel.getQuestions().size());
+        
+        getSequence(testModel.getQuestions().size());
+        questions = testModel.getQuestions();
         updateQuestion(questions.get(sequenceQuestions.get(0)));
         initButtons();
-    }
-    
-    public void initQuestions(){
-        //System.out.println("initQuestions");
-        englishTestModel.addQuestion(new Question("Cual es la pregunta1", new String[]{"Nada1", "Si hay1", "no1", "Ya1"}, "/Imagenes/pregunta (1).png", 0, 0));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta2", new String[]{"Nada2", "Si hay2", "no2", "Ya2"}, "/Imagenes/confuso.png", 1, 2));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta3", new String[]{"Nada3", "Si hay3", "no3", "Ya3"}, "/Imagenes/unicornio.png", 2, 3));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta4", new String[]{"Nada4", "Si hay4", "no4", "Ya4"}, "/Imagenes/idea.png", 3, 0));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta5", new String[]{"Nada5", "Si hay5", "no5", "Ya5"}, "/Imagenes/pensando.png", 4, 0));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta6", new String[]{"Nada6", "Si hay6", "no6", "Ya6"}, "/Imagenes/idea (1).png", 5, 3));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta7", new String[]{"Nada7", "Si hay7", "no7", "Ya7"}, "/Imagenes/pensar.png", 6, 1));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta8", new String[]{"Nada8", "Si hay8", "no8", "Ya8"}, "/Imagenes/profesor.png", 7, 1));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta9", new String[]{"Nada9", "Si hay9", "no9", "Ya9"}, "/Imagenes/reina.png", 8, 3));
-        englishTestModel.addQuestion(new Question("Cual es la pregunta10", new String[]{"Nada10", "Si hay10", "no10", "Ya10"}, "/Imagenes/reino-unido.png", 9, 2));
-        //System.out.println("initQuestions2");
     }
     
     public void initButtons(){
@@ -125,9 +111,9 @@ public class CQuestions {
     
     private FrmQuestions view;
     private Question model;
-    private Test englishTestModel = new Test();
+    private Test testModel = new Test();
     private JButton[] btnAnswers= new JButton[4];
-    private ArrayList<Question> questions = englishTestModel.getQuestions();
+    private ArrayList<Question> questions = null;
     private Set<Integer> sortedQuestions = new HashSet<>();
     private ArrayList<Integer> sequenceQuestions = new ArrayList<>();
     private int n = 1;
