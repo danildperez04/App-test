50802f748d71de2a6b97f3ed24dc63b79d9297eb
Galileo <galileoblandon1@gmail.com>
2022-06-26 15:36:09 -0600

    Agregue lo de los test completados

--- src/Controller/CMenu.java
+++ src/Controller/CMenu.java
@@ -54,16 +54,17 @@ public class CMenu {
         FrmQuestions frmQuestions = new FrmQuestions();
         Question model = new Question();
         Test englishTest = new Test();
+        CResults.setTypeTest("A1");
         englishTest.addQuestion(new Question("A1", new String[]{"Esta es la respuesta", "Si hay1", "no1", "Ya1"}, "/Imagenes/pregunta (1).png", 0, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta2", new String[]{"Nada2", "Si hay2", "no2", "Ya2"}, "/Imagenes/confuso.png", 1, "2", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta3", new String[]{"Nada3", "Si hay3", "no3", "Ya3"}, "/Imagenes/unicornio.png", 2, "3", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta4", new String[]{"Nada4", "Si hay4", "no4", "Ya4"}, "/Imagenes/idea.png", 3, "4", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta5", new String[]{"Nada5", "Si hay5", "no5", "Ya5"}, "/Imagenes/pensando.png", 4, "5", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta6", new String[]{"Nada6", "Si hay6", "no6", "Ya6"}, "/Imagenes/idea (1).png", 5, "6", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta7", new String[]{"Nada7", "Si hay7", "no7", "Ya7"}, "/Imagenes/pensar.png", 6, "7", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta8", new String[]{"Nada8", "Si hay8", "no8", "Ya8"}, "/Imagenes/profesor.png", 7, "8", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta9", new String[]{"Nada9", "Si hay9", "no9", "Ya9"}, "/Imagenes/reina.png", 8, "9", "https://www.britishcouncil.es/ingles/niveles/a1"));
-        englishTest.addQuestion(new Question("Cual es la pregunta10", new String[]{"Nada10", "Si hay10", "no10", "Ya10"}, "/Imagenes/reino-unido.png", 9, "10", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta2", new String[]{"Esta es la respuesta", "Si hay2", "no2", "Ya2"}, "/Imagenes/confuso.png", 1, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta3", new String[]{"Esta es la respuesta", "Si hay3", "no3", "Ya3"}, "/Imagenes/unicornio.png", 2, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta4", new String[]{"Esta es la respuesta", "Si hay4", "no4", "Ya4"}, "/Imagenes/idea.png", 3, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta5", new String[]{"Esta es la respuesta", "Si hay5", "no5", "Ya5"}, "/Imagenes/pensando.png", 4, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta6", new String[]{"Esta es la respuesta", "Si hay6", "no6", "Ya6"}, "/Imagenes/idea (1).png", 5, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta7", new String[]{"Esta es la respuesta", "Si hay7", "no7", "Ya7"}, "/Imagenes/pensar.png", 6, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta8", new String[]{"Esta es la respuesta", "Si hay8", "no8", "Ya8"}, "/Imagenes/profesor.png", 7, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta9", new String[]{"Esta es la respuesta", "Si hay9", "no9", "Ya9"}, "/Imagenes/reina.png", 8, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
+        englishTest.addQuestion(new Question("Cual es la pregunta10", new String[]{"Esta es la respuesta", "Si hay10", "no10", "Ya10"}, "/Imagenes/reino-unido.png", 9, "Esta es la respuesta", "https://www.britishcouncil.es/ingles/niveles/a1"));
         CQuestions cQuestions = new CQuestions(frmQuestions, model, englishTest);
         view.dispose();
         frmQuestions.setVisible(true);
@@ -74,6 +75,7 @@ public class CMenu {
         FrmQuestions frmQuestions = new FrmQuestions();
         Question model = new Question();
         Test englishTest = new Test();
+        CResults.setTypeTest("A2");
         englishTest.addQuestion(new Question("A2", new String[]{"Nada1", "correcta", "no1", "Ya1"}, "/Imagenes/pregunta (1).png", 0, "correcta", "https://www.britishcouncil.es/ingles/niveles/a1"));
         englishTest.addQuestion(new Question("Cual es la pregunta2", new String[]{"Nada2", "Si hay2", "no2", "Ya2"}, "/Imagenes/confuso.png", 1, "", "https://www.britishcouncil.es/ingles/niveles/a1"));
         englishTest.addQuestion(new Question("Cual es la pregunta3", new String[]{"Nada3", "Si hay3", "no3", "Ya3"}, "/Imagenes/unicornio.png", 2, "", "https://www.britishcouncil.es/ingles/niveles/a1"));
@@ -87,6 +89,8 @@ public class CMenu {
         CQuestions cQuestions = new CQuestions(frmQuestions, model, englishTest);
         view.dispose();
         frmQuestions.setVisible(true);
+        Cronometro c = new Cronometro();
+        c.Cronometro();
     }
 
     private void B1MouseClicked(java.awt.event.MouseEvent evt) {
@@ -94,6 +98,7 @@ public class CMenu {
         FrmQuestions frmQuestions = new FrmQuestions();
         Question model = new Question();
         Test englishTest = new Test();
+        CResults.setTypeTest("B1");
         englishTest.addQuestion(new Question("Cual es la pregunta1", new String[]{"Nada1", "Si hay1", "no1", "Ya1"}, "/Imagenes/pregunta (1).png", 0, "", "https://www.britishcouncil.es/ingles/niveles/a1"));
         englishTest.addQuestion(new Question("Cual es la pregunta2", new String[]{"Nada2", "Si hay2", "no2", "Ya2"}, "/Imagenes/confuso.png", 1, "", "https://www.britishcouncil.es/ingles/niveles/a1"));
         englishTest.addQuestion(new Question("Cual es la pregunta3", new String[]{"Nada3", "Si hay3", "no3", "Ya3"}, "/Imagenes/unicornio.png", 2, "", "https://www.britishcouncil.es/ingles/niveles/a1"));
@@ -114,6 +119,7 @@ public class CMenu {
         FrmQuestions frmQuestions = new FrmQuestions();
         Question model = new Question();
         Test englishTest = new Test();
+        CResults.setTypeTest("B2");
         englishTest.addQuestion(new Question("Cual es la pregunta1", new String[]{"Nada1", "Si hay1", "no1", "Ya1"}, "/Imagenes/pregunta (1).png", 0, "", "https://www.britishcouncil.es/ingles/niveles/a1"));
         englishTest.addQuestion(new Question("Cual es la pregunta2", new String[]{"Nada2", "Si hay2", "no2", "Ya2"}, "/Imagenes/confuso.png", 1, "", "https://www.britishcouncil.es/ingles/niveles/a1"));
         englishTest.addQuestion(new Question("Cual es la pregunta3", new String[]{"Nada3", "Si hay3", "no3", "Ya3"}, "/Imagenes/unicornio.png", 2, "", "https://www.britishcouncil.es/ingles/niveles/a1"));
